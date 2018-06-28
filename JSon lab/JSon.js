65c6a50fbe63f11b7d4ae2858ff186a3d46a04a4
Natasha Mae Ignacio <nignacio@nignacio-mn1.linkedin.biz>
2018-06-28 14:50:45 -0700

    Create JSon file

---
+++ JSon lab/JSon.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    function processForm(e) {
+ 
+ 
+      e.preventDefault();
+ 
+      var data = document.getElementById("JSon").value;
+  
+ 
+ 
+    }
+ var form = document.getElementById("JSon");
+ form.addEventListener("submit", processForm);
+ }
